31daacd09fb240e6f225639a9b727e0970373305
davidg91 <davidg91@stanford.edu>
2015-06-08 14:28:47 -0700

    added splash screen and music

---
+++ Assets/Scripts/ImageDisplay.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ImageDisplay : MonoBehaviour {
+	public Texture splashImage;
+	public Texture homeScreen;
+	public float timeToDisplaySplash;
+	public int nextLevelToLoad;
+	public AudioClip themeMusic;
+	
+	private float timeForNextLevel;
+	
+	public void Start() {
+		timeForNextLevel = Time.time + timeToDisplaySplash;
+	}
+	
+	public void OnGUI() {
+		GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), splashImage);
+		if (Time.time >= timeForNextLevel) {
+			GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), homeScreen);
+			if (Input.GetKeyDown ("space"))
+				Application.LoadLevel(nextLevelToLoad);
+		}
+	}
+}
